6474ae3c68da8ac6cd8c74c8f46cc90e3e8a6fbb
Varun Sharma <vsofficialmail@gmail.com>
2022-08-22 18:30:48 +0530

    Initial Commit

---
+++ pystore-mobile/pystore_mobileApp.swift
@@ -0,0 +1,20 @@
+//
+//  pystore_mobileApp.swift
+//  pystore-mobile
+//
+//  Created by Varun Sharma on 22/08/22.
+//
+
+import SwiftUI
+
+@main
+struct pystore_mobileApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
